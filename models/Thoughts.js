32e7369e3dc9a96fc0ed0445b6d05c42bd74743e
xChrisxWilliamsx <williamsc0325@gmail.com>
2024-09-09 16:33:28 -0700

    Adds models

---
+++ models/Thoughts.js
@@ -0,0 +1,60 @@
+const { Schema, model } = require('mongoose');
+// const Reaction = require('./Reactions');
+
+const reactionsSchema = new Schema(
+    {
+        id: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        timestamps: { 
+            createdAt: 'created_at', 
+            updatedAt: 'updated_at' 
+        },
+    }
+);
+
+const thoughtsSchema = new Schema(
+    {
+        text: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        timestamps: { 
+            createdAt: 'created_at', 
+            updatedAt: 'updated_at' 
+        },
+        reactions: [reactionsSchema]
+    },
+    {
+        toJSON: {
+          virtuals: true,
+        },
+        toObject:{
+            virtuals: true,
+        }
+    }
+);
+
+thoughtsSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thoughts = model('thoughts', thoughtsSchema);
+
+module.exports = Thoughts;
---
+++ models/index.js
@@ -0,0 +1,6 @@
+const Users = require('./Users');
+const Thoughts = require('./Thoughts');
+// const Reactions = require('./Reactions');
+
+// Reactions 
+module.exports = { Users, Thoughts };
