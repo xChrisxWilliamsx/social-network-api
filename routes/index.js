1d51201fa45611207e83b9d487b2028e4ddcd45c
xChrisxWilliamsx <williamsc0325@gmail.com>
2024-09-10 00:54:58 -0700

    Adds back wrong route status

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
-// router.use((req, res) => {
-//   return res.send('OOOPS These are not the routes your looking for!');
-// });
+router.use((req, res) => {
+  return res.send('OOOPS These are not the routes your looking for!');
+});
 
 module.exports = router;
